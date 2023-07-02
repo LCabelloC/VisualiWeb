885768a4c00d19fc0ec1d1d2ca657879e8782c3b
LCabelloC <lcabelloms@gmail.com>
2023-07-02 19:08:15 +0200

    New NavBar

--- src/Componentes/Elements/NavBar.js
+++ src/Componentes/Elements/NavBar.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Css/NavBar.css";
-import Logo from "../Assets/VisualiSmallLogo.png";
 
 const NavBar = () => {
-    return (
-        <nav>
-            <div className="logo">
-                <a href="#mainSection">
-                    <img src={Logo} alt="Company logo" />
-                </a>
-            </div>
-            <div className="navLinks">
-                <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">DECUBRE EL SOFTWARE</a></li>
-                    <li><a href="#">¿QUIÉNES SOMOS?</a></li>
-                    <li><a href="#">FAQ</a></li>
-                </ul>
-            </div>
-           <div className="joinButton">
-                <a href="#">ÚNETE</a>
-           </div>
-        </nav>
-    );
-}
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const scrollThreshold = windowHeight * 0.1;
+
+      if (scrollPosition >= scrollThreshold) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`nav ${isFixed ? "fixed" : ""}`}>
+      <div className="logo">
+        <a href="#mainSection"></a>
+      </div>
+      <div className="navLinks">
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">DECUBRE EL SOFTWARE</a></li>
+          <li><a href="#">¿QUIÉNES SOMOS?</a></li>
+          <li><a href="#">FAQ</a></li>
+        </ul>
+      </div>
+      <div className="joinButton">
+        <a href="#">ÚNETE</a>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
