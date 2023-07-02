c9f8b54a6a75beb759bce2ce1a1062d8e00655d0
javiercubas <javiercubas2003@gmail.com>
2023-07-02 14:15:24 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './Componentes/Elements/NavBar';
-import MainSection from './Componentes/Elements/MainSection';
+import NavBar from './componentes/Elements/NavBar';
+import MainSection from './componentes/Elements/MainSection';
+import Header from './componentes/Elements/Header';
 
 function App() {
   return (
     <>
+    <div className="mainsection-div">
+          <MainSection/>
+    </div>
       <header>
         <NavBar />
       </header>
       <main>
-        <MainSection/>
+        <Header />
       </main>
       <footer>
 
