5dba4d1bceb97ffe1cc4632d4d1d575f96c2b470
LCabelloC <lcabelloms@gmail.com>
2023-07-02 13:20:18 +0200

    NavBar añadido

--- src/Componentes/Elements/NavBar.js
+++ src/Componentes/Elements/NavBar.js
@@ -2,8 +2,18 @@ import React from "react";
 import "../Css/NavBar.css";
 
 const NavBar = () => {
-    return(
+    return (
         <nav>
+            <div className="logo" />
+            <div className="navLinks">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">DECUBRE EL SOFTWARE</a></li>
+                    <li><a href="#">¿QUIÉNES SOMOS?</a></li>
+                    <li><a href="#">ÚNETE</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
