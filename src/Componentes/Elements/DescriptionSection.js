1d1b9ea9f5f194b79635c589ba25c504b0871020
LCabelloC <lcabelloms@gmail.com>
2023-07-02 19:25:48 +0200

    Updating before pull

--- src/Componentes/Elements/DescriptionSection.js
+++ src/Componentes/Elements/DescriptionSection.js
@@ -6,8 +6,25 @@ const DescriptionSection = () => {
     <section id="DescriptionSection">
         <div className="description-mockup"></div>
         <div className="description-right">
-          <h2>Transforma tu experiencia de transmisión digital</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam ultricies, nunc sapien aliquet nibh, vitae aliquam nisl nunc id nunc. Donec euismod, nisl sed aliquam ultricies, nunc sapien aliquet nibh, vitae aliquam nisl nunc id nunc.</p>
+          <h2> Transformando tu experiencia de visualización digital</h2>
+          <p>Visuali es una innovadora plataforma que revoluciona la forma en que
+            disfrutas del contenido digital. Con Visuali, podrás crear tu propia
+            experiencia visual personalizada, diseñando la pantalla que desees y
+            seleccionando el contenido que más te interese. ¡Imagina tener el
+            control total sobre lo que ves y cómo lo ves!
+
+            Pero eso no es todo. Visuali te ofrece una inteligencia artificial avanzada
+            que estará a tu disposición para brindarte recomendaciones personalizadas basadas
+            en tus intereses. No tendrás que buscar contenido, ¡el contenido te encontrará a
+            ti! Además, la IA de Visuali puede generar resúmenes de tus experiencias, para
+            que puedas compartir fácilmente tus momentos favoritos con otros.
+
+            Con Visuali, disfrutarás de una experiencia de visualización digital única y
+            cautivadora. Ya no estarás limitado por las restricciones de las plataformas
+            tradicionales, sino que podrás explorar un nuevo mundo de posibilidades y
+            conectar con las personas que más te importan.
+
+            ¡Descubre Visuali y sumérgete en una nueva era de visualización digital!</p>
           <button className='header-button'>Descargar</button>
         </div>
     </section>
