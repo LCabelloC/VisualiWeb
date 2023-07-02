5e56e00fe574b5ff9e941ff69e80d3c0a4c7607a
LCabelloC <lcabelloms@gmail.com>
2023-07-02 19:23:10 +0200

    Updating before pull

--- src/Componentes/Elements/DescriptionSection.js
+++ src/Componentes/Elements/DescriptionSection.js
@@ -4,10 +4,10 @@ import "../Css/DescriptionSection.css";
 const DescriptionSection = () => {
   return (
     <section id="DescriptionSection">
-        <div className="header-mockup"></div>
-        <div className="header-right">
-          <h2 className='header-slogan'>La app del futuro ya está aqui</h2>
-          <p className='header-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam ultricies, nunc sapien aliquet nibh, vitae aliquam nisl nunc id nunc. Donec euismod, nisl sed aliquam ultricies, nunc sapien aliquet nibh, vitae aliquam nisl nunc id nunc.</p>
+        <div className="description-mockup"></div>
+        <div className="description-right">
+          <h2>Transforma tu experiencia de transmisión digital</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam ultricies, nunc sapien aliquet nibh, vitae aliquam nisl nunc id nunc. Donec euismod, nisl sed aliquam ultricies, nunc sapien aliquet nibh, vitae aliquam nisl nunc id nunc.</p>
           <button className='header-button'>Descargar</button>
         </div>
     </section>
