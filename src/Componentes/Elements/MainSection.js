1fc702c3c94a4fa641258d45732a9c8c59b840f3
javiercubas <javiercubas2003@gmail.com>
2023-07-03 10:39:20 +0200

    update

--- src/Componentes/Elements/MainSection.js
+++ src/Componentes/Elements/MainSection.js
@@ -2,9 +2,9 @@ import React from "react";
 import "../Css/MainSection.css";
 
 const MainSection = () => {
-    return(
+    return (
         <section id="mainSection">
-            <div className="companyLogo"/>
+            <div className="companyLogo" />
             <h1>TRANSMISIONES DE VIDEO PERSONALIZADAS</h1>
             <p>VISUALI te ofrece una experiencia de visualización digital sin igual. Personaliza tus
                 pantallas, elige tu contenido favorito y conecta con las personas que más te importan.
@@ -12,6 +12,10 @@ const MainSection = () => {
                 resúmenes instantáneos. Descubre una nueva forma de ver contenido digital con Visuali.
                 ¡Únete ahora y abre tus ojos a un mundo de posibilidades visuales!
             </p>
+
+            <div className="joinButton2">
+                <a href="#">ÚNETE</a>
+            </div>
         </section>
     )
 };
