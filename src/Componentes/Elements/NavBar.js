c1b9d878f36fa8a5ddb9bf8466285905a7e780b0
javiercubas <javiercubas2003@gmail.com>
2023-07-03 13:20:28 +0200

    menu movil

--- src/Componentes/Elements/NavBar.js
+++ src/Componentes/Elements/NavBar.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FaTimes } from 'react-icons/fa';
 import "../Css/NavBar.css";
 
 const NavBar = () => {
   const [isFixed, setIsFixed] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,20 +28,30 @@ const NavBar = () => {
   }, []);
 
   return (
-    <nav className={`nav ${isFixed ? "fixed" : ""}`}>
+    <nav className={`nav ${isFixed ? "fixed" : ""} ${isMenuOpen ? "menu-open" : ""}`}>
       <div className="logo">
         <a href="#mainSection"></a>
       </div>
-      <div className="navLinks">
-        <ul>
-          <li><a href="#mainSection">HOME</a></li>
-          <li><a href="#DescriptionSection">DESCUBRE EL SOFTWARE</a></li>
-          <li><a href="#teamSection">¿QUIÉNES SOMOS?</a></li>
-          <li><a href="#">FAQ</a></li>
-        </ul>
+      <div className="navAll">
+        <div className="movil-title">
+          <h2>Menu principal</h2>
+          <FaTimes size={32} color="var(--logo)" onClick={() => setIsMenuOpen(!isMenuOpen)} />
+        </div>
+        <div className="navLinks">
+          <ul>
+            <li><a href="#mainSection">HOME</a></li>
+            <li><a href="#DescriptionSection">DESCUBRE EL SOFTWARE</a></li>
+            <li><a href="#teamSection">¿QUIÉNES SOMOS?</a></li>
+            <li><a href="#">FAQ</a></li>
+          </ul>
+        </div>
+        <div className="joinButton">
+          <a href="#">ÚNETE</a>
+        </div>
       </div>
-      <div className="joinButton">
-        <a href="#">ÚNETE</a>
+
+      <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <AiOutlineMenu size={32} color="var(--text)" />
       </div>
     </nav>
   );
