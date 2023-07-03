7b81d765b9761ffa5be6d096c979de91db61a732
javiercubas <javiercubas2003@gmail.com>
2023-07-03 11:20:10 +0200

    update

--- src/Componentes/Elements/Persona.js
+++ src/Componentes/Elements/Persona.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { FaLinkedin } from 'react-icons/fa';
 
 const Persona = (props) => {
     const { nombre, foto, puesto, descripcion, url } = props;
     return (
         <div className="persona">
-            <a href={url} target="_blank" rel="noopener noreferrer" className='foto' style={{ backgroundImage: `url(${foto})` }} />
-            <h3 className='persona-nombre'>{nombre}</h3>
+            <div className='foto' style={{ backgroundImage: `url(${foto})` }} />
+            <a href={url} target="_blank" rel="noopener noreferrer" className="nombre-linkedin">
+                <h3 className='persona-nombre'>{nombre}</h3>
+                <FaLinkedin size={22} color="var(--text)" className='linkedin' />
+            </a>
             <h4 className='persona-puesto'>{puesto}</h4>
             <div className="persona-desc">
                 <p className='persona-estudios'>Ingeniería Informática</p>
