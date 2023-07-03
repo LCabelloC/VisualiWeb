0c37ee5b3b0090af2f65dc58a2261abe38c88daa
LCabelloC <lcabelloms@gmail.com>
2023-07-03 18:09:46 +0200

    Solving one type error

--- src/Componentes/Elements/MainSection.js
+++ src/Componentes/Elements/MainSection.js
@@ -9,7 +9,7 @@ const MainSection = () => {
             <p><span>VISUALI</span> te ofrece una experiencia de visualización digital sin igual. <span>Personaliza</span> tus
                 pantallas, <span>elige</span> tu contenido favorito y <span>conecta</span> con las personas que más te importan.
                 Nuestra <span>inteligencia artificial</span> te sorprenderá con <span>recomendaciones personalizadas</span> y
-                <span>resúmenes instantáneos</span>. Descubre una nueva forma de ver contenido digital con Visuali.
+                <span> resúmenes instantáneos</span>. Descubre una nueva forma de ver contenido digital con Visuali.
                 ¡<span>Únete ahora</span> y abre tus ojos a un mundo de posibilidades visuales!
             </p>
 
